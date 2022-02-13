a7ad71db1e646f625353be7e1a25ab62eea1065b
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2022-02-13 23:06:18 +0200

    Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,6 @@ export const App = () => {
   stats={user.stats}
 />
 <Statistics title="Upload stats" stats={data} />
-<Statistics stats={data} />
     </div>
   );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,33 +1,38 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import './Statistics.css';
-// import randomColor from "randomcolor";
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from'./Statistics.module.css';
+import randomColor from "randomcolor";
 
 
-// function Statistics({}){
-//     return(
-// <section class="statistics">
-//   <h2 class="title">Upload stats</h2>
+function Statistics({title, stats}){
+    return(
+<section className={s.statistics}>
+{title && <h2 className={s.title}>{title}</h2>}
 
-//   <ul class="stat-list">
-//     <li class="item">
-//       <span class="label">.docx</span>
-//       <span class="percentage">{percentage}</span>
-//     </li>
-//     <li class="item">
-//       <span class="label">.mp3</span>
-//       <span class="percentage">{percentage}</span>
-//     </li>
-//     <li class="item">
-//       <span class="label">.pdf</span>
-//       <span class="percentage">{percentage}</span>
-//     </li>
-//     <li class="item">
-//       <span class="label">.mp4</span>
-//       <span class="percentage">{percentage}</span>
-//     </li>
-//   </ul>
-// </section>        
+  <ul className={s.list}>
+  {stats.map(({ id, label, percentage }) => (
+          <li
+            key={id}
+            className={s.item}
+            style={{ backgroundColor: onRandomColor() }}
+          >
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}%</span>
+          </li>
+        ))}
+  </ul>
+</section>        
 
-//     );
-// }
+    );
+}
+function onRandomColor() {
+    return "#" + Math.floor(Math.random() * 16777216).toString(16);
+  }
+Statistics.propTypes = {
+    title: PropTypes.string,
+    id: PropTypes.string,
+    label: PropTypes.string,
+    percentage: PropTypes.number,
+  };
+  
+export default Statistics;
